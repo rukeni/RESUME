8eecdcbd551094b411e4b7f65baed293a620ccdb
SeungMin Yang <rukeni@naver.com>
2020-06-12 16:44:11 +0900

    리팩토링: absolute 추가

--- src/domain/Home/Card.jsx
+++ src/domain/Home/Card.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Card = () => (
-  <div className="flex-1 flex justify-center items-center">
+  <div className="flex-1 flex w-full p-6 justify-center items-center relative z-50 hover:bg-transparent">
+    <button
+      className="absolute"
+      type="button"
+      style={{
+        top: '0',
+        right: '0',
+      }}
+    >
+      적용
+    </button>
     <span>인간의 욕심은 끝이 없고... 나의 욕심도 끝이 없다</span>
   </div>
 );
