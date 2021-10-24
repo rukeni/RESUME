55e40bbdc16752112af53be42d3e879d49ae364d
양탕구리 <rukeni@naver.com>
2021-10-24 22:00:27 +0900

    Remove : 기존 소스 제거

--- src/net.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const WAKATIME_API_ROOT = 'https://wakatime.com/api/v1/';
-export const WAKA_CODING_STATS = 'https://wakatime.com/share/@4d629d74-e8c0-40fd-96f9-d13120c37b45/5af4b3a9-87cb-429f-a521-02d715e0d5b7.json';
-export const GITHUB_STATUS = 'https://api.github.com/users/rukeni';
