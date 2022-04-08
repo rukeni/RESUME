f86f1af42ff659a9d6958cf16ef7de9607055852
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-04-08 22:16:27 +0900

    Modify : 사이드바 생성, 폰트 설정, web audio api 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import type { Component } from 'solid-js';
 
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
+import music from './assets/mercedes.mp3'
 import styles from './App.module.css';
 import Sidebar from './components/sidebar/Sidebar';
 
@@ -8,11 +9,14 @@ const App: Component = () => {
   return (
     <div class='bg-indigo-500 w-full h-screen flex'>
       <Sidebar />
-      <header class='bg-red-500 flex-1'>
+      
+      <header class='bg-[#f5f5f5] flex-1 pl-4'>
+        <div>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <audio src={music} controls crossorigin="anonymous" ></audio>
         <a
           class='btn'
           href="https://github.com/solidjs/solid"
@@ -21,6 +25,19 @@ const App: Component = () => {
         >
           Learn Solid
         </a>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        <p class='gg'>한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용한글 테스트는 아주 쉽지요용</p>
+        </div>
       </header>
     </div>
   );
---
+++ src/components/sidebar/SidebarItem.tsx
@@ -0,0 +1,56 @@
+const SidebarItem = () => {
+	return (
+		<div class='w-full flex flex-col mt-8'>
+			<a
+				class='sidebar-item relative py-2 px-5  text-lg font-bold w-full leading-8 bg-primary text-center text-black bg-transparent border-y border-solid duration-300 cursor-pointer box-border border-neutral-100 text-neutral-800 hover:border-b hover:border-solid hover:border-neutral-100 hover:bg-zinc-100 hover:px-5 hover:py-2 hover:text-center hover:text-lg hover:text-neutral-800 focus:text-cyan-800'
+				href='#about_me'
+				data-animation='59'
+				data-goto='2'
+				style='text-decoration: none; outline: none;'>
+				Home
+			</a>
+			<a
+				class='sidebar-item relative py-2 px-5  text-lg font-bold w-full leading-8 bg-primary text-center bg-transparent border-b border-solid duration-300 cursor-pointer box-border border-neutral-100 text-neutral-800 hover:border-b hover:border-solid hover:border-neutral-100 hover:bg-zinc-100 hover:px-5 hover:py-2 hover:text-center hover:text-lg hover:text-neutral-800 focus:text-cyan-800'
+				href='#about_me'
+				data-animation='59'
+				data-goto='2'
+				style='text-decoration: none; outline: none;'>
+				About me
+			</a>
+			<a
+				class='sidebar-item relative py-2 px-5  text-lg font-bold w-full leading-8 bg-primary text-center bg-transparent border-b border-solid duration-300 cursor-pointer box-border border-neutral-100 text-neutral-800 hover:border-b hover:border-solid hover:border-neutral-100 hover:bg-zinc-100 hover:px-5 hover:py-2 hover:text-center hover:text-lg hover:text-neutral-800 focus:text-cyan-800'
+				href='#about_me'
+				data-animation='59'
+				data-goto='2'
+				style='text-decoration: none; outline: none;'>
+				Resume
+			</a>
+			<a
+				class='sidebar-item relative py-2 px-5  text-lg font-bold w-full leading-8 bg-primary text-center bg-transparent border-b border-solid duration-300 cursor-pointer box-border border-neutral-100 text-neutral-800 hover:border-b hover:border-solid hover:border-neutral-100 hover:bg-zinc-100 hover:px-5 hover:py-2 hover:text-center hover:text-lg hover:text-neutral-800 focus:text-cyan-800'
+				href='#about_me'
+				data-animation='59'
+				data-goto='2'
+				style='text-decoration: none; outline: none;'>
+				Portfolio
+			</a>
+			<a
+				class='sidebar-item relative py-2 px-5  text-lg font-bold w-full leading-8 bg-primary text-center bg-transparent border-b border-solid duration-300 cursor-pointer box-border border-neutral-100 text-neutral-800 hover:border-b hover:border-solid hover:border-neutral-100 hover:bg-zinc-100 hover:px-5 hover:py-2 hover:text-center hover:text-lg hover:text-neutral-800 focus:text-cyan-800'
+				href='#about_me'
+				data-animation='59'
+				data-goto='2'
+				style='text-decoration: none; outline: none;'>
+				Blog
+			</a>
+			<a
+				class='sidebar-item relative py-2 px-5  text-lg font-bold w-full leading-8 bg-primary text-center bg-transparent border-b border-solid duration-300 cursor-pointer box-border border-neutral-100 text-neutral-800 hover:border-b hover:border-solid hover:border-neutral-100 hover:bg-zinc-100 hover:px-5 hover:py-2 hover:text-center hover:text-lg hover:text-neutral-800 focus:text-cyan-800'
+				href='#about_me'
+				data-animation='59'
+				data-goto='2'
+				style='text-decoration: none; outline: none;'>
+				Contact
+			</a>
+		</div>
+	);
+};
+
+export default SidebarItem;
