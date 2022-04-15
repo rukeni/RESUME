12c7ef524763867772158d8bcff17d7206bf699b
양탕구리 <rukeni@naver.com>
2022-04-15 19:18:05 +0900

    Add : 랜덤 숫자 생성 함수 추가

--- src/app/helper.ts
+++ src/app/helper.ts
@@ -1,3 +1,8 @@
-export const getData = () => {
-  console.log('data')
+export const getRandomInt = (min:number, max:number) => {
+  if(Math.min(min, max) === max) {
+    [min, max] = [max, min]
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
 }
---
+++ src/app/ponicode/helper.test.ts
@@ -0,0 +1,18 @@
+import * as helper from "../helper"
+// @ponicode
+describe("helper.getRandomInt", () => {
+    test("0", () => {
+        let result: any = helper.getRandomInt(undefined, undefined)
+        expect(result).toBe(NaN)
+    })
+
+    test("1", () => {
+        let result: any = helper.getRandomInt(-100, 0)
+        expect(result).toBe(-70)
+    })
+
+    test("2", () => {
+        let result: any = helper.getRandomInt(0, -1)
+        expect(result).toBe(-1)
+    })
+})
